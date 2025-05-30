4f608a795306f4c518fedd5b8bae391fda0cffc6
bernabarros <bernardobarros.work@gmail.com>
2025-05-30 10:19:33 +0100

    Create MultipleStuff project

---
+++ MultipleStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleStuff
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
